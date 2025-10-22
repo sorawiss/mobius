cd0944110452f641ad3279a4e3de235a3e02e8f9
NatthidaSae <Natthida.sae@ascendcorp.com>
2025-10-22 18:17:47 +0700

    replace app icons with new mobius logo

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
@@ -1,2 +1,5 @@
+// Use a local public asset so swapping the logo is as simple as replacing the file.
+// Place your brand square PNG at `public/images/icons/android/android-launchericon-192-192.png`.
+// This path is also used as the default in the auth Logo component, keeping things consistent.
 export const DEFAULT_WORKSPACE_LOGO =
-  'https://twentyhq.github.io/placeholder-images/workspaces/twenty-logo.png';
+  '/images/icons/android/android-launchericon-192-192.png';
