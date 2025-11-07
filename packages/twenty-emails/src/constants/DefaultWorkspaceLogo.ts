230302e6e863a1ef1d54f5c0650b02c269dbc04c
NaTThy0330 <Focusywd@outlook.com>
2025-11-07 19:17:03 +0700

    add mail template Logo

--- packages/twenty-emails/src/constants/DefaultWorkspaceLogo.ts
+++ packages/twenty-emails/src/constants/DefaultWorkspaceLogo.ts
@@ -1,2 +1,12 @@
-export const DEFAULT_WORKSPACE_LOGO =
+const DEFAULT_CDN_WORKSPACE_LOGO =
   'https://app.mobius.com/images/icons/android/android-launchericon-192-192.png';
+
+const DEFAULT_SERVER_LOGO_PATH = '/public/images/icons/mobius-email-logo.png';
+
+const normalizedServerUrl = process.env.SERVER_URL
+  ? process.env.SERVER_URL.replace(/\/$/, '')
+  : undefined;
+
+export const DEFAULT_WORKSPACE_LOGO = normalizedServerUrl
+  ? `${normalizedServerUrl}${DEFAULT_SERVER_LOGO_PATH}`
+  : DEFAULT_CDN_WORKSPACE_LOGO;
