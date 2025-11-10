aa21f197f7c028f0231bf513ab43719933a7bb57
NaTThy0330 <Focusywd@outlook.com>
2025-11-10 22:31:41 +0700

    fix email reset pass logo

--- packages/twenty-emails/src/constants/DefaultWorkspaceLogo.ts
+++ packages/twenty-emails/src/constants/DefaultWorkspaceLogo.ts
@@ -3,10 +3,15 @@ const DEFAULT_CDN_WORKSPACE_LOGO =
 
 const DEFAULT_SERVER_LOGO_PATH = '/public/images/icons/mobius-email-logo.png';
 
+const customLogoUrl = process.env.EMAIL_LOGO_URL?.trim();
+
 const normalizedServerUrl = process.env.SERVER_URL
   ? process.env.SERVER_URL.replace(/\/$/, '')
   : undefined;
 
-export const DEFAULT_WORKSPACE_LOGO = normalizedServerUrl
-  ? `${normalizedServerUrl}${DEFAULT_SERVER_LOGO_PATH}`
-  : DEFAULT_CDN_WORKSPACE_LOGO;
+export const DEFAULT_WORKSPACE_LOGO =
+  customLogoUrl && customLogoUrl.length > 0
+    ? customLogoUrl
+    : normalizedServerUrl
+      ? `${normalizedServerUrl}${DEFAULT_SERVER_LOGO_PATH}`
+      : DEFAULT_CDN_WORKSPACE_LOGO;
